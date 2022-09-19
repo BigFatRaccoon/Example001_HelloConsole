b258290737fa305e30bdd8136672c729fa30565b
Iliya Palych <iliya-paly4@yandex.ru>
2022-09-19 22:58:33 +0400

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Палыч")
+{
+    Console.WriteLine("Да это же Палыч!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
